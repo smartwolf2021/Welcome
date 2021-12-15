974d7531dff921aad2e87215af88cf6c831bc45d
OlaVasileva <vasileva_ola@mail.ru>
2021-12-15 19:49:22 +0300

    Initial Commit

---
+++ Welcome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Welcome
+//
+//  Created by Васильева Ольга on 15.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
