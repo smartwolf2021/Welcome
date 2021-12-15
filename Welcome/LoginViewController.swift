b8ad3ce4a7d3249869c90388833c6c36b6c4f7ce
OlaVasileva <vasileva_ola@mail.ru>
2021-12-15 23:21:09 +0300

    The end

--- Welcome/LoginViewController.swift
+++ Welcome/LoginViewController.swift
@@ -8,22 +8,55 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard let userName = userNameTextField.text, !(userNameTextField.text?.isEmpty ?? false) else {
+            showAlert()
+            return
+        }
+        
+        switch segue.identifier {
+        case "toWelcomeScreen":
+            prepareWelcomeScreen(segue, userName: userName)
+        default:
+            break
+        }
+    }
+    
+    @IBAction func unwindToLoginScreen(_ segue: UIStoryboardSegue) {}
+    
+    func clearTextFields() {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    private func prepareWelcomeScreen(_ segue: UIStoryboardSegue, userName: String) {
+        guard let destinationController = segue.destination as? WelcomeViewController else {
+            return
+        }
+        destinationController.userName = userName
+    }
+    
+    private func showAlert() {
+        let alert = UIAlertController(title: "Ошибка!", message: "Не указано имя!", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "ОК", style: .default)
+        alert.addAction(alertAction)
+        present(alert, animated: true, completion: nil)
+    }
 
-    /*
+    
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
--- Welcome/WelcomeViewController.swift
+++ Welcome/WelcomeViewController.swift
@@ -9,21 +9,33 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var userName: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        welcomeLabel.text = "Welcome, \(userName)"
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        switch segue.identifier {
+        case "toLoginScreen":
+            prepareLoginScreen(segue)
+        default:
+            break
+        }
     }
-    */
+    
+    private func prepareLoginScreen(_ segue: UIStoryboardSegue) {
+        guard let destinationController = segue.destination as? LoginViewController else {
+            return
+        }
+        destinationController.clearTextFields()
+    }
+
+    // MARK: - Navigation
 
 }
